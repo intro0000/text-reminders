9cd4cdfeb4c32c013a1099a74b786cadf2c2b058
intro0000 <hadionoc@gmail.com>
2022-07-20 21:59:37 -0400

    add more media options

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,13 +2,16 @@ class NotificationsController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     @@gifs = [
-        "https://i.imgur.com/oVL5IV1.gifv",
+        "https://i.imgur.com/oVL5IV1.gif",
         "https://media0.giphy.com/media/MAuWrJTykDWvUlZ8Ef/giphy.gif?cid=ecf05e471id5f05rlkwaezugheppcxiedwvf9uqu3kztjw02&rid=giphy.gif&ct=g",
         "https://i.imgur.com/4eJVw0b.gif",
         "https://media4.giphy.com/media/3oEduTc1ImDHt8hoJy/giphy.gif?cid=ecf05e47x8dg28rz0mnvgurceqw45ztns6qnz75pyksc3f6v&rid=giphy.gif&ct=g",
         "https://media4.giphy.com/media/eSwGh3YK54JKU/giphy.gif?cid=ecf05e47i1zac8hdgx6dr82sohmxbshyaozekpm12hxduq73&rid=giphy.gif&ct=g",
         "https://media0.giphy.com/media/2QHLYZFJgjsFq/giphy.gif?cid=ecf05e47vuwzcgidfjcxzgnnck5rmgqg58n4rijig5eo073s&rid=giphy.gif&ct=g",
-        "https://media4.giphy.com/media/E9zwx0N8EPvyw/giphy.gif?cid=ecf05e47d81dwvrkzpc5cgeoxr2areuvitjho9edr8mh2qo7&rid=giphy.gif&ct=g"
+        "https://media4.giphy.com/media/E9zwx0N8EPvyw/giphy.gif?cid=ecf05e47d81dwvrkzpc5cgeoxr2areuvitjho9edr8mh2qo7&rid=giphy.gif&ct=g",
+        "https://media1.giphy.com/media/7JkdvxxsXXeskAxyjT/giphy.gif?cid=ecf05e47n5u4zje5va13jcgykruukijznq7m3t12hzgxav7t&rid=giphy.gif&ct=g",
+        "https://i.imgur.com/9Olh1Dn.gif",
+        "https://c.tenor.com/P9pPZ1prRCMAAAAd/pokemon-pikachu.gif"
     ]
 
     def notify
